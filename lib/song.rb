c07924e7de975770448e6f0a36b11723237bb6f2
Morgan Fudge <mfudge92@gmail.com>
2017-11-13 19:32:32 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,8 @@ class Song
   end
 
   def self.new_by_filename(name)
-    title = name.chomp(".mp3").split(" - ")[1]
-    Song.new(title)
+    title = name.chomp(".mp3").split(" - ")
+    song = Song.new(title[1])
+    artist = Artist.find_or_create_by_name(title[0])
   end
 end

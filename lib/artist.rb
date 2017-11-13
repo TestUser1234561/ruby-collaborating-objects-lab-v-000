2e50e23a19e4b53b60a34ab2506eae82e9b65c90
Morgan Fudge <mfudge92@gmail.com>
2017-11-13 19:21:52 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,6 +12,10 @@ class Artist
     @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
   def self.find_or_create_by_name(name)
     artist = @@all.select {|o| o.name == name}
     if(!artist.nil?)

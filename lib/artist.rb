f06e3ed476c3f506a6c89fa7effbe6ae2d8a0b94
Morgan Fudge <mfudge92@gmail.com>
2017-11-13 19:35:29 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,7 +19,7 @@ class Artist
   def self.find_or_create_by_name(name)
     artist = @@all.select {|o| o.name == name}
     if(!artist.nil?)
-      return artist[0]
+      return artist.first
     end
     return Artist.new(name)
   end
